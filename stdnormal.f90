46d819ad1ddfc1c8b9ab6b4676930138ab4019a3
Somajit <dey.somajit@gmail.com>
2023-03-14 23:14:20 +0530

    Added provision for shift and scale using boolean arg 'force'

--- stdnormal.f90
+++ stdnormal.f90
@@ -3,26 +3,37 @@ module stdnormal
     
     contains
     
-! Std normal variate generation using box-muller transform
-! But not using polar rejection or trigonometric functions
- subroutine random_stdnormal(z) 
+! Generates an array of std. normal variates using Box-Muller transform
+! Does not use rejection sampling or trigonometric functions
+! If 'force' is present and is .true., the variates are shifted and scaled to produce perfect mean and s.d.
+ subroutine random_stdnormal(z, force)
+    logical, intent(in), optional :: force
     double precision, dimension(:), intent(out) :: z
     double precision, dimension(2*((size(z)+1)/2)), target :: u ! Always of even size >= size(z)
     double precision, dimension(:), pointer :: u1, u2
-    integer :: u1_size,diff_z_size
+    integer :: z_size, u1_size, z_diff_u1_size
+    double precision :: z_mean, z_sd
 
     call random_number(u) ! Harvest uniform random number from [0,1]
     u = 2*u-1 ! Transform into uniform random number from [-1,1]
 
     u1 => u(:size(u)/2) ! u1 refers to the first half of array u
+    z_size = size(z)
     u1_size = size(u1) ! Also equals the size of u2 by the way
-    diff_z_size = size(z) - u1_size
+    z_diff_u1_size = z_size - u1_size
     u2 => u(u1_size+1:) ! u2 refers to the second half of array u
     
     z(:u1_size) = 2/(1 + u2*u2)
-    z(u1_size+1:) = sign(z(:diff_z_size) - 1, u1(:diff_z_size))
+    z(u1_size+1:) = sign(z(:z_diff_u1_size) - 1, u1(:z_diff_u1_size))
     u1 = sqrt(-2*log(abs(u1)))
     z(:u1_size) = z(:u1_size)*u2*u1
-    z(u1_size+1:) = z(u1_size+1:)*u1(:diff_z_size)
+    z(u1_size+1:) = z(u1_size+1:)*u1(:z_diff_u1_size)
+    
+    shift_and_scale: if (present(force)) then
+        if (.not. force) exit shift_and_scale
+        z_mean = sum(z)/z_size
+        z_sd = sqrt(sum(z*z)/z_size - z_mean*z_mean)
+        z = (z - z_mean)/z_sd
+    end if shift_and_scale
 end subroutine random_stdnormal
 end module stdnormal
